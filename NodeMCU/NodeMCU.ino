961bec79f514c2d2d4a1451ddf26459c99c99202
Trân Hồng Quân <hongquan95.tran@gmail.com>
2018-03-21 00:09:07 +0700

    finish test status 2 led

--- NodeMCU/NodeMCU.ino
+++ NodeMCU/NodeMCU.ino
@@ -5,33 +5,30 @@
 char auth[] = "3dddac1594e74646bde292060be39113";    //AuthToken copy ở Blynk Project
 char ssid[] = "TOP";  //Tên wifi
 char pass[] = "0968457018";     //Mật khẩu wifi
+int x, y;
 void setup()
 {
   Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
 }
 
-BLYNK_WRITE(V3)
+void readStt()
 {
-  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
-  // You can also use:
-  // String i = param.asStr();
-  // double d = param.asDouble();
-//  Serial.print("V3 Slider value is: ");
-//  Serial.println((pinValue+9999)/19.5);
+ x = digitalRead(D2);
+ y = digitalRead(D3);
+}
+void writeStt(int x, int y)
+{
+  Serial.print("Led 1 is: ");
+  Serial.println(x);
+  Serial.print("Led 2 is: ");
+  Serial.println(y);
+  delay(100);
 }
 
 void loop()
 {
   Blynk.run();
-//  float x = digitalRead(D3);
-//  delay(200);
-//  if ( x < 1)
-//   {
-//    Serial.print(x);
-//    Blynk.email("hongquan95.tran@gmail.com", "Subject: Nhan email ne", "Email toi roi ne");
-//    Blynk.notify("Hey, Blynkers! My hardware can push now!");
-//    delay(10000);
-//   }
-   
+  readStt();
+  writeStt(x,y);
 }
