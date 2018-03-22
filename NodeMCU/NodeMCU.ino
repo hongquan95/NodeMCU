746a32146b1d6839c786150e795150a590b7d34e
Trân Hồng Quân <hongquan95.tran@gmail.com>
2018-03-22 11:08:55 +0700

    test DHT 11

--- NodeMCU/NodeMCU.ino
+++ NodeMCU/NodeMCU.ino
@@ -1,47 +1,40 @@
-#define BLYNK_PRINT Serial    
+
+#define BLYNK_PRINT Serial
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <SimpleTimer.h>
+#include <DHT.h>
 
 char auth[] = "3dddac1594e74646bde292060be39113";    //AuthToken copy ở Blynk Project
-char ssid[] = "TOP";  //Tên wifi
-char pass[] = "0968457018";     //Mật khẩu wifi
+//char ssid[] = "TOP";  //Tên wifi
+//char pass[] = "0968457018";     //Mật khẩu wifi
+
+char ssid[] = "NHATRO98";  //Tên wifi
+char pass[] = "568568568";     //Mật khẩu wifi
+
 int x, y, z;
+#define DHTPIN D5         // Pin ket noi voi DHT
+#define DHTTYPE DHT11     // Su dung cam bien DHT11
+DHT dht(DHTPIN, DHTTYPE); // Cau hinh chan DHT
+
 void setup()
 {
   Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
+  dht.begin();                          // Khoi tao DHT
 }
-
-void readStt()
+BLYNK_READ(V0) //Blynk app has something on V5
 {
- x = digitalRead(D2);
- y = digitalRead(D3);
- z = digitalRead(D4);
-}
-
-BLYNK_WRITE(V4)
-{
-  String i = param.asStr();
-  Serial.print("STT is: ");
-  Serial.println(i);
-  delay(200);
+  float h = dht.readHumidity();     //Doc gia tri do am
   
-  if (i == "Quan")
-  { 
-     readStt();
-     Blynk.email("You Stt", "Led 1 is:" + String(x) + " Led 2 is:" + String(y) + " Led 3 is:" + String(z));
-     Serial.println("Email is sent")
-     Blynk.virtualWrite(V4,"checked");
-     delay(200);
-  }
+  Serial.println("Do am:" + String(h));
+  Blynk.virtualWrite(V0, h); //sending to Blynk
 }
-void writeStt(int x, int y)
+BLYNK_READ(V1) //Blynk app has something on V5
 {
-  Serial.print("Led 1 is: ");
-  Serial.println(x);
-  Serial.print("Led 2 is: ");
-  Serial.println(y);
-  delay(100);
+  float t = dht.readTemperature();  //Doc gia tri nhiet do
+  Serial.print("Nhiet do:" + String(t));
+  Blynk.virtualWrite(V1, t); //sending to Blynk
 }
 
 void loop()
